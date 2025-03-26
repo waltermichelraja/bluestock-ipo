d7589828c22ccc3480d50ac0cbc29bcc952550ff
Walter Michel Raja <85942041+waltermichelraja@users.noreply.github.com>
2025-03-26 12:48:15 +0530

    patch update users 0.2

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('profile', update_user_profile, name='update_user_profile'),
     path('watchlist', get_watchlist, name='get_watchlist'),
     path('watchlist/add', add_to_watchlist, name='add_to_watchlist'),
-    path('watchlist/<int:stockId>/remove', remove_from_watchlist, name='remove_from_watchlist'),
+    path('watchlist/<int:stockId>/delete', remove_from_watchlist, name='remove_from_watchlist'),
 ]
